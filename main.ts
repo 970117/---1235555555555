37b38a21bd1b8c441eaf330c19423818fc51d515
EricLin <91246944+970117@users.noreply.github.com>
2021-11-18 16:11:32 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,152 @@
-let list = [
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    list = [
+    [
+    1,
+    1,
+    1,
+    0,
+    1
+    ],
+    [
+    0,
+    0,
+    0,
+    1,
+    0
+    ],
+    [
+    1,
+    0,
+    1,
+    1,
+    1
+    ],
+    [
+    1,
+    1,
+    0,
+    0,
+    0
+    ],
+    [
+    1,
+    0,
+    0,
+    1,
+    0
+    ]
+    ]
+    for (let y = 0; y <= 4; y++) {
+        for (let x = 0; x <= 4; x++) {
+            if (list[y][x]) {
+                led.plot(x, y)
+            } else {
+                led.unplot(y, x)
+            }
+        }
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    list = [
+    [
+    0,
+    0,
+    1,
+    0,
+    1
+    ],
+    [
+    1,
+    1,
+    0,
+    0,
+    1
+    ],
+    [
+    1,
+    0,
+    1,
+    1,
+    1
+    ],
+    [
+    0,
+    0,
+    0,
+    0,
+    1
+    ],
+    [
+    1,
+    0,
+    1,
+    1,
+    1
+    ]
+    ]
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (list[y][x]) {
+                led.plot(y, x)
+            } else {
+                led.unplot(y, x)
+            }
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    list = [
+    [
+    1,
+    0,
+    1,
+    1,
+    1
+    ],
+    [
+    0,
+    1,
+    0,
+    1,
+    0
+    ],
+    [
+    1,
+    0,
+    0,
+    0,
+    0
+    ],
+    [
+    1,
+    1,
+    0,
+    0,
+    1
+    ],
+    [
+    1,
+    0,
+    0,
+    0,
+    1
+    ]
+    ]
+    for (let x = 0; x <= 4; x++) {
+        for (let y = 0; y <= 4; y++) {
+            if (list[y][x]) {
+                led.plot(y, x)
+            } else {
+                led.unplot(y, x)
+            }
+        }
+    }
+})
+let list: number[][] = []
+list = [
 [
 1,
 0,
@@ -35,9 +183,8 @@ let list = [
 1
 ]
 ]
-for (let y = 0; y <= 4; y++) {
-    for (let index = 0; index <= 4; index++) {
-        let x = 0
+for (let x = 0; x <= 4; x++) {
+    for (let y = 0; y <= 4; y++) {
         if (list[y][x]) {
             led.plot(x, y)
         } else {
